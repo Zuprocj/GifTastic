e47e0c519e310061ba432d62d6c3d07274a7aaef
zuprocj <jmdcorpuz@gmail.com>
2019-09-04 10:43:52 -0700

    added on click event to buttons, qurey url and api key. and added fixed height photos of the gifs.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,3 +11,19 @@ var tags = ["hamburger", "bacon", "fried chicken", "cake", "soda", "cookies"];
     tags.push(newTag);
     $("#tags").append('<button class="tag-buttons btn btn-primary">' + newTag + '</button>');
   });
+  $(document).on('click', '.tag-buttons', function(event) {
+    event.preventDefault();
+    var type = this.innerText;
+    console.log(this.innerText);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + window.encodeURI(type) + "&limit=10&api_key=FMOHyA4DrkLZ6iIW5c6XkJugc0fgoYsY";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      for (var i = 0; i < response.data.length; i++) {
+        $("#photo").append('<img class="gif" src="' + response.data[i].images.fixed_height_still.url + '">');
+    }  
+});
+$("#photo").empty();
+});
+renderButtons();
