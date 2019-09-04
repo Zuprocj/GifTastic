11e52907d6396ed281901724f623b3ff3d3afafb
zuprocj <jmdcorpuz@gmail.com>
2019-09-04 10:28:42 -0700

    added way to created new tags and add to the tag list

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,3 +5,9 @@ var tags = ["hamburger", "bacon", "fried chicken", "cake", "soda", "cookies"];
       $("#tags").append('<button class="tag-buttons btn btn-primary">' + tags[i] + '</button>');
     }      
   } 
+  $(document).on('click', '#addTag', function(event) {
+    event.preventDefault();
+    var newTag = $("#category").val().trim();
+    tags.push(newTag);
+    $("#tags").append('<button class="tag-buttons btn btn-primary">' + newTag + '</button>');
+  });
